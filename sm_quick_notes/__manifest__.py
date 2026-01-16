f63bb897268f4faa03f21fe8abc67b019428cc82
stevenmarp <stevenmarp@sinerka.id>
2026-01-17 00:47:51 +0700

    SM Quick Notes v16.0.1.0.0 - Initial release for Odoo 16.0

--- sm_quick_notes/__manifest__.py
+++ sm_quick_notes/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'SM Quick Notes',
-    'version': '18.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Productivity',
     'summary': 'Sticky notes widget in systray for quick note-taking',
     'description': """
